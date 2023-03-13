63b7133ad83dcffb696f6fa7d1efef1423f5969c
MeriGL <127746080+MeriGL@users.noreply.github.com>
2023-03-13 15:46:57 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{ 
+int numero = 42;
+double numerolungo = 5436374847;
+float numeroconlavirgola = 5.47;
+char carattere = 'c';
+
+printf("il numero è: %d\n", numero);
+printf("numero lungo è: %f, numero con la virgola è: %f\n", numerolungo, numeroconlavirgola);
+printf ("il carattere è %c\n" , carattere);
+}
