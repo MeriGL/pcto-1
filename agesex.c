8dc522b982b76efebb3bcd9287768072f0fa53e4
MeriGL <127746080+MeriGL@users.noreply.github.com>
2023-03-14 10:17:18 +0100

    Add files via upload

---
+++ agesex.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+int main ()
+
+{
+int eta;
+char sex;
+
+printf("inserisci etá e sesso ");
+scanf("%d %c", &eta, &sex);
+printf("il numero e %d, il sesso %c\n" , eta , sex);
+
+
+}
